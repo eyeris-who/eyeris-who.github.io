c0770f7ef08767869f79ef18a6f9f0ad8eec5cb3
eyeris-who <92889912+eyeris-who@users.noreply.github.com>
2021-11-06 08:27:00 -0700

    Update uploader.php

--- uploader.php
+++ uploader.php
@@ -1,19 +1,3 @@
-<?php
+<?php $ path = "files/"; $path = $path . basename( $_FILES['userfile']['name']);
 
-// Where the file is going to be placed 
-$target_path = "/uploads/";
-
-/* Add the original filename to our target path.  
-Result is "uploads/filename.extension" */
-$target_path = $target_path . basename( $_FILES['uploadedfile']['name']); 
-
-
-
-if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
-    echo "The file ".  basename( $_FILES['uploadedfile']['name']). 
-    " has been uploaded";
-} else{
-    echo "There was an error uploading the file, please try again!";
-}
-
-?>
+if(move_uploaded_file($_FILES['userfile']['tmp_name'], $path)) { echo "Success uploading". basename($_FILES['userfile']['name']); } else{ echo "Error when uploading file."; } ?>
